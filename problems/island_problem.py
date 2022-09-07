3a134bc331a3939e95f16d5f157884ea421ba977
yineric <yinzheng163@gmail.com>
2022-09-07 09:05:41 +0800

    update

--- problems/island_problem.py
+++ problems/island_problem.py
@@ -1,4 +1,3 @@
-
 """
 https://leetcode.cn/problems/number-of-islands/solution/dao-yu-lei-wen-ti-de-tong-yong-jie-fa-dfs-bian-li-/
 输入：grid = [
@@ -13,6 +12,8 @@ https://leetcode.cn/problems/number-of-islands/solution/dao-yu-lei-wen-ti-de-ton
 链接：https://leetcode.cn/problems/number-of-islands
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+
+
 def numIslands(self, grid: List[List[str]]) -> int:
     if not grid:
         return 0
@@ -33,3 +34,26 @@ def numIslands(self, grid: List[List[str]]) -> int:
                 landNum += 1
                 Dfs(grid, i, j)
     return landNum
+
+"""
+给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+
+说明：每次只能向下或者向右移动一步。
+https://leetcode.cn/problems/minimum-path-sum/
+"""
+def minPathSum(self, grid: List[List[int]]) -> int:
+    if not grid or not grid[0]:
+        return 0
+
+    rows, columns = len(grid), len(grid[0])
+    dp = [[0] * columns for _ in range(rows)]
+    dp[0][0] = grid[0][0]
+    for i in range(1, rows):
+        dp[i][0] = dp[i - 1][0] + grid[i][0]
+    for j in range(1, columns):
+        dp[0][j] = dp[0][j - 1] + grid[0][j]
+    for i in range(1, rows):
+        for j in range(1, columns):
+            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+
+    return dp[rows - 1][columns - 1]
