9a6bdb70cbdd1f47e4114964d4efc559d08139f6
yineric <yinzheng163@gmail.com>
2022-09-25 23:59:05 +0800

    update

--- interview/kaidu/ma_solution.py
+++ interview/kaidu/ma_solution.py
@@ -11,8 +11,14 @@ class MovingAverage:
         """
         :param num_bin:
         :param window:
-        window_sum: sum value of the windows. remove head elem and append the tail elem
-        queue: head --> tail, if head.timestamp < tail.timestamp - window: remove(head)
+        self.window_sum: sum value of the windows. remove head elem and append the tail elem
+        self.queue: 双端队列保存窗口内的elem，elem的格式含义如下， elem: [current_timestamp, current_value, current_area]
+                    current_timestamp: 当前时间戳, current_value: 当前股价, current_area: 当前股价与间隔的积分面积
+                    current_area的值需要下一个elem进来之后通过该公式得出并更新: current_area = current_value * (next_timestamp - prev_timestamp)
+                    由于频繁的队首队尾部操作，考虑使用双端队列，首尾操作时间复杂度O(1)
+        self.window_sum: 窗口内元素current_area的总和, 维护window_sum变量用来避免频繁计算队列的和，时间复杂度可从O(n)优化至->O(1)
+        self.prev_mean: 每次流数据进入一个元素，根据当前元素的时间戳，前一个元素的 prev_area = prev_value * (current_timestamp - prev_timestamp)
+                        self.prev_mean = self.window_sum / (最后一个元素的时间戳 - 第一个元素的时间戳)
         """
         self.num_bin = num_bin
         self.window = window
@@ -26,6 +32,7 @@ class MovingAverage:
         if self.queue == 0:
             return 0
         else:
+            # 线程安全加读写锁的读锁
             read_marker = marker.gen_rlock()
             read_marker.acquire()
             current_ts = time.time() * 1000 if not current_ts else current_ts
@@ -40,41 +47,90 @@ class MovingAverage:
             read_marker.release()
             return mean_value
 
+    """
+    1.如何淘汰W窗口头
+    2.如何淘汰num_bin，把prev和curr合并，area加到前面的元素
+    """
+
     def Update(self, timestamp: float, value: float):
         "新数据到达，更新状态"
+        # 线程安全加读写锁的写锁
         write_marker = marker.gen_wlock()
         write_marker.acquire()
+        """
+        初始化操作：
+        1. 每一次新元素到达更新前一个时间戳的股价面积积分 current_area
+        2. 忘队尾部插入元素 elem = [timestamp, value, None] ， 第三个股价积分面积area初始化为None
+        """
         if len(self.queue) == 0:
             prev_area = 0
         else:
             interval = timestamp - self.queue[-1][0]
             prev_value = self.queue[-1][1]
             prev_area = prev_value * interval
+            # 每一次新元素到达更新前一个时间戳的股价面积积分 current_area
             self.queue[-1][2] = prev_area
-        # [timestamp, current_value, current_area]
-        # current_area will be updated when the next value reach
+
         elem = [timestamp, value, None]
         self.queue.append(elem)
+        """
+        Window时间窗口淘汰策略:
+        在窗口内判断双端队列队首的时间戳是否过期，考虑到数据粒度间隔不均匀的情况，如果直接删掉队首会造成均值突然变化比较大的情况。
+        例如: 假设大部分元素都是毫秒粒度，但队首元素持续超过1个小时后，下一个元素才来，这样队首的股价积分面积
+        由于前后时间戳差距变大非常巨大(current_area = current_value * (next_timestamp - prev_timestamp))
+        所以分情况讨论：
+        先计算溢出时间戳长度diff_ts = timestamp(当前时间戳) - self.window(窗口时间) - head_ts(队首时间戳) 
+        diff_ts的长度计算需要扣除的 reduce_area (需要扣除的股价积分面积)，reduce_area = 队首元素股价 * diff_ts
+        队首剩余股价积分面积 remain_area = head_area - reduce_area
+        1. 如果remain_area > 0, 说明队首的积分面积还可以再减，就扣除reduce_area, 保证window_sum随着新元素到来一点一点扣除
+                               扣减后队首的时间戳相应平移 diff_ts 长度, 不断扣减直到队首的时间戳不过期位置
+        2. 如果remain_area <=0, 说明队首的积分面积不够扣减，则直接弹出队首元素，将整个剩余股价积分面积扣减掉
+        self.window_sum = self.window_sum - total_reduce_area + prev_area
+        """
         head_ts, tail_ts = self.queue[0][0], self.queue[-1][0]
-        if head_ts <= timestamp - self.window:
-            head = self.queue.popleft()
-            head_area = head[2]  # area_i = value_i * interval
+        if head_ts < timestamp - self.window:
+            total_reduce_area = 0
+            while head_ts < timestamp - self.window:
+                diff_ts = timestamp - self.window - head_ts
+                reduce_area = self.queue[0][1] * diff_ts
+                if self.queue[0][2] - reduce_area > 0:
+                    self.queue[0][2] -= reduce_area  # 队首股价积分面积需要扣除溢出长度的股价积分面积
+                    self.queue[0][0] += diff_ts  # 扣减后队首的时间戳相应平移 diff_ts 长度，保证队列中元素都在window范围内
+                else:
+                    head = self.queue.popleft()
+                    reduce_area = head[2]  # area_i = value_i * interval
+                total_reduce_area += reduce_area
+                head_ts = self.queue[0][0]
         else:
-            head_area = 0
-        self.window_sum = self.window_sum - head_area + prev_area
+            total_reduce_area = 0
+
+        self.window_sum = self.window_sum - total_reduce_area + prev_area
+        total = sum(list(map(lambda x: x[2], self.queue))[:-1])
+        if abs(total - self.window_sum) > 0.000001:
+            print()
+
         if len(self.queue) <= 1:
             self.prev_mean = self.queue[0][1]
         else:
             self.prev_mean = self.window_sum / (self.queue[-1][0] - self.queue[0][0])
 
+        """
+        内存不足合并策略：
+        
+        """
         if len(self.queue) >= self.num_bin:
             diff = inf
             for idx, item in enumerate(self.queue):
-                timestamp, value, area = item[0], item[1], item[2]
-                cur_diff = abs(value - self.prev_mean)
-                if cur_diff < diff:
-                    diff = cur_diff
-                    elem = [timestamp, value, area]
+                if idx >= 1 and idx < len(self.queue) - 1:
+                    timestamp, value, area = item[0], item[1], item[2]
+                    prev_timestamp, prev_value, prev_area = self.queue[idx - 1][0], self.queue[idx - 1][1], \
+                                                            self.queue[idx - 1][2]
+                    cur_diff = abs(value - self.prev_mean)
+                    if cur_diff < diff:
+                        diff = cur_diff
+                        elem = [timestamp, value, area]
+                        prev_elem = [prev_timestamp, prev_value, prev_area]
+
             self.queue.remove(elem)
             self.window_sum -= elem[2]
             self.prev_mean = self.window_sum / (self.queue[-1][0] - self.queue[0][0])
--- interview/kaidu/mock_case.py
+++ interview/kaidu/mock_case.py
@@ -1,13 +1,14 @@
 import time
+import unittest
 
 from interview.kaidu.ma_solution import MovingAverage
 
 mock_data = [
     (1360250000000.00, 15.07),
-    (1360250000001.00, 14.89),
-    (1360250000002.00, 14.45),
-    (1360250000003.00, 14.3),
-    (1360250000004.00, 14.94),
+    # (1360250000001.00, 14.89),
+    # (1360250000002.00, 14.45),
+    # (1360250000003.00, 14.3),
+    # (1360250000004.00, 14.94),
     (1360250000005.00, 13.93),
     (1360250000006.00, 14.33),
     (1360250000007.00, 14.17),
@@ -27,27 +28,54 @@ mock_data = [
     (1360250000021.00, 15.14),
     (1360250000022.00, 15.54),
     (1360250000023.00, 15.98),
-    (1360250000024.00, 16.45),
-    (1360250000025.00, 15.8),
-    (1360250000026.00, 16.48),
-    (1360250000027.00, 17.13),
-    (1360250000028.00, 17.21),
-    (1360250000029.00, 17.1),
+    # (1360250000024.00, 16.45),
+    # (1360250000025.00, 15.8),
+    # (1360250000026.00, 16.48),
+    # (1360250000027.00, 17.13),
+    # (1360250000028.00, 17.21),
+    # (1360250000029.00, 17.1),
     (1360250000030.00, 16.92)
 ]
 
+
+class CaseTest(unittest.TestCase):
+    # 测试Window窗口内sum值,用于测试队窗口移动过程中，过期元素时序粒度不均匀与插入元素时序粒度不均匀两种情况
+    def _test_window_sum(self):
+        num_bin = 100
+        window = 15.0
+        ma = MovingAverage(num_bin=num_bin, window=window)
+        # 取最终窗口的元素，从第11个元素开始到倒数第二个元素，排除最后一个元素是因为最后一个元素不参与积分面积计算)
+        actual_window_sum = sum(list(map(lambda x: x[1], mock_data[11:-1])))
+        # 按照前一个股价插值, 补了6个值
+        actual_window_sum += 15.98 * (1360250000030.00 - 1360250000023.00 - 1)
+        ma.MockTask(mock_data=mock_data)
+        # 对比实际值与窗口值
+        assert abs(ma.window_sum - actual_window_sum) < 0.000000000001
+        self.assertAlmostEqual(ma.window_sum, actual_window_sum)
+
+    # 测试Window窗口内mean值
+    def _test_mean(self):
+        num_bin = 100
+        window = 15.0
+        ma = MovingAverage(num_bin=num_bin, window=window)
+        # 取最终窗口的元素，从第11个元素开始到倒数第二个元素，排除最后一个元素是因为最后一个元素不参与积分面积计算)
+        actual_window_sum = sum(list(map(lambda x: x[1], mock_data[11:-1])))
+        # 按照前一个股价插值, 补了6个值
+        actual_window_sum += 15.98 * (1360250000030.00 - 1360250000023.00 - 1)
+        actual_window_mean = actual_window_sum / (len(mock_data[11:-1]) + 6)
+        ma.MockTask(mock_data=mock_data)
+        assert abs(ma.prev_mean - actual_window_mean) < 0.000000000001
+        self.assertAlmostEqual(ma.prev_mean, actual_window_mean)
+
+    def test_num_bin(self):
+        num_bin = 10
+        window = 15.0
+        ma = MovingAverage(num_bin=num_bin, window=window)
+        ma.MockTask(mock_data=mock_data)
+        print()
+
 if __name__ == '__main__':
-    '''
-    file_path = '/Users/yineric/Downloads/archive/all_stocks_5yr.csv'
-    t = pd.read_csv(file_path, usecols=['date', 'open']).head(2)
-    '''
-    from interview.kaidu.mock_case import mock_data
-    num_bin = 10
-    window = 15.0
-    ma = MovingAverage(num_bin=num_bin, window=window)
-    start_time = time.time()
-    ma.MockTask(mock_data=mock_data[:20])
-    res = ma.Get(current_ts=1360250000001.99)
-    end_time = time.time()
-    print('time consume: {}'.format(end_time - start_time))
-    print()
+    ct = CaseTest()
+    ct._test_num_bin()
+
+
