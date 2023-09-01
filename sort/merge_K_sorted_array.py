eeba97492e3536607f9219bab56b4c0ea83ee00a
yineric <yinjialai@pinduoduo.com>
2023-09-01 09:26:50 +0800

    update

--- sort/merge_K_sorted_array.py
+++ sort/merge_K_sorted_array.py
@@ -3,7 +3,6 @@ class Solution:
     @param arrays: k sorted integer arrays
     @return: a sorted array
     """
-
     def mergekSortedArrays(self, arrays):
         # write your code here
         import heapq
